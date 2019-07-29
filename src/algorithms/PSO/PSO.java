8f87aaf66aea4481637e11c6816d2b106616a962
mitk13@gmail.com <318229366Me>
2019-07-29 20:03:00 +0300

    init swarm

--- src/algorithms/PSO/PSO.java
+++ src/algorithms/PSO/PSO.java
@@ -16,10 +16,7 @@ public class PSO implements IEvolutionaryGroup, IOptimizationAlgorithm {
     @Override
     public BufferedImage recreateFromPrimitive(BufferedImage original, EShapeType shape) {
         BufferedImage image = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
-        Graphics2D graphics = image.createGraphics();
-        graphics.setColor(ImageUtils.getAvgColor(original));
-        graphics.fillRect(0, 0, original.getWidth(), original.getHeight());
-        graphics.dispose();
+
         for (int shapesIndex = 0; shapesIndex < MAX_SHAPES; shapesIndex++) {
             System.out.println("Shapes: " + shapesIndex);
             swarm = new Swarm(original, image, shape);
