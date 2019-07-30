3dc308f4e676fe13947c410ec440c5c37b38eae1
mitk13@gmail.com <318229366Me>
2019-07-30 21:53:56 +0300

    lower number of particles

--- src/algorithms/PSO/PSOConstants.java
+++ src/algorithms/PSO/PSOConstants.java
@@ -9,7 +9,7 @@ public class PSOConstants {
     public static final String FILE_SPLIT_FORMAT = "\\.";
     public static final String IMAGE_FORMAT = "png";
     public static final String IMAGE_SUFFIX = "_primitive";
-    static final int MAX_PARTICLES = 60;
+    static final int MAX_PARTICLES = 50;
     static final int VELOCITY_FACTOR = 10;
     public static final double W = 0.4;
     public static final double C1 = 2;
